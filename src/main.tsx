9b937b113380c924d12df40ea9cc49e4df79672d
neicore <neicoreadams@gmail.com>
2022-11-28 02:00:04 +0300

    add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  { path: '/register', element: <Register /> },
+  { path: '/login', element: <Login /> },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <>
+      <div>Cool landing page goes here</div>
+      <a href="/login">Login</a>
+      <a href="/register">Register</a>
+    </>
+  )
+}
+export default Home
